8a7eaaa38c462e9bd1e8b0490ff666bcc1b03a19
Dominykas Anuzis <dominykasanuzis@gmail.com>
2022-07-19 11:45:26 +0300

    done

--- src/main/java/school/mjc/stage0/loops/finalTask/Pyramid.java
+++ src/main/java/school/mjc/stage0/loops/finalTask/Pyramid.java
@@ -2,6 +2,17 @@ package school.mjc.stage0.loops.finalTask;
 
 public class Pyramid {
     public void printPyramid(int cathetusLength) {
+        for (int i = 1; i <= cathetusLength; i++){
+
+            for (int j = 1; j < cathetusLength * 2; j++){
+                if(j >= cathetusLength - i + 1 && j <= cathetusLength + i - 1){
+                    System.out.print((cathetusLength - j + 1) > 0 ? (cathetusLength - j + 1) : (cathetusLength - j + 1) * -1 + 2);
+                } else {
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
 
     }
 
